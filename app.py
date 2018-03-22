b18fd82839c69ef55baa2a9da26607337dddc8f1
Gisela1003 <gisela.str.23@gmail.com>
2018-03-22 16:22:00 -0600

    first commit

--- app.py
+++ app.py
@@ -55,8 +55,8 @@ def processRequest(req):
     #baseurl = "https://query.yahooapis.com/v1/public/yql?"
     baseurl = "https://fernanda@0123@www.consistent.com.mx/api/jsonws/alexaskill.entry/add-query/state-id/41107/nombre-estado/Aguascalientes/anio/0"
     #yql_query = makeYqlQuery(req)
-    if yql_query is None:
-        return {}
+    #if yql_query is None:
+    #    return {}
     #yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
     yql_url = baseurl
     result = urlopen(yql_url).read()
